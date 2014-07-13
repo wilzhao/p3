b27e085bd9ea6cd1e9d848194590b4cb868ca5db
wilzhao <wzhaomt454@gmail.com>
2014-07-13 01:53:16 -0400

    figured out how to use stylesheets

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -2,10 +2,14 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	
+	{{HTML::style('stylesheets/style.css');}}
+
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:500' rel='stylesheet' type='text/css'>
+	
 @yield('head')
 </head>
 <body>
-
 @yield('content')
 </body>
 </html>
